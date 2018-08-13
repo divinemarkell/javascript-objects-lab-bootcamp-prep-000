5f0ab0c931d49c0e41f50781670c10c0cb63fa8c
Markell Owens <markellowens@mac.com>
2018-08-13 21:13:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function updateObjectWithKeyAndValue (obj, key, value) {
+  obj[key] = value
+  return obj
+}
+const recipes = {lemon: 3}
+function updateObjectWithKeyAndValue (recipes, 'water', '8 cups') {
+  
+}
